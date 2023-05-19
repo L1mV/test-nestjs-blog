930914c3c7ca5b2aeb5163f8fa7b7217a9e04e90
l1m <l1mv@outlook.com>
2023-05-20 06:18:59 +0800

    add notFound page css file

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,5 @@
+import styles from '../styles/404.module.css'
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return <h1 className={styles['not-found']}>404 - Page Not Found</h1>
 }
